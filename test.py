1d844ddff6c6dc56371456dba88b4b8fab8414d8
PRANEETH KOTTE <p.kotte.22@abdn.ac.uk>
2023-03-04 09:44:40 +0000

    commit message

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
-def test_datausa():
-  response=app.get('/')
-  assert response.status_code==200
-  asser response.data==b'datausa'
+import unittest
+import datausa.py
+from app import app
+
+class TestApp(unittest.TestCase):
+  def SetUp(self):
+    app.testing=True
+    self.app=app.test_client()
+
+
+  def test_index(self):
+    response=self.app.get('/')
+    self.assertEqual(response.status_code,200)
+
+
+
+
+  def test_datausa():
+    response=self.app.get('/')
+    self.assertEqual(response.status_code,200)
+    self.assertEqual(response.data,b'datausa')
+
+if __name__=='__main__':
+  unittest.main()
